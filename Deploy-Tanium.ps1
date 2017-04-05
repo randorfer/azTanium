8005f3655648b3ec5585d57a1c0c9b24711b2a2e
Ryan Andorfer <ryan.andorfer@MLREM1711665.local>
2017-04-05 09:49:00 -0500

    move networking out of compute deploy

--- Deploy-Tanium.ps1
+++ Deploy-Tanium.ps1
@@ -22,16 +22,29 @@ param(
 
 $ErrorActionPreference = "Stop"
 
-$TemplateFile = './scenarios/tanium/azuredeploy.json'
-$TemplateParameterObject = @{
+# If changed this needs to be updated in the loadBalancer.parameters.json file to match for the compute deployment...
+$NetworkingResourceGroupName = 'CORPNET'
+$NetworkingTemplateFile = './scenarios/tanium/networking.azuredeploy.json'
+
+$ComputeTemplateFile = './scenarios/tanium/compute.azuredeploy.json'
+$ComputeTemplateParameterObject = @{
     'deploymentSize' = $DeploymentSize
 }
 # Login to Azure and select your subscription
 Login-AzureRmAccount -SubscriptionId $SubscriptionId | Out-Null
 
+# Create Networking Resource Group
+New-AzureRmResourceGroup -Name $NetworkingResourceGroupName -Location $Location -Force | Out-Null
+
+# Deploy Networking
+New-AzureRmResourceGroupDeployment -Name "TaniumDeployNetworking" `
+                                   -ResourceGroupName $NetworkingResourceGroupName `
+                                   -TemplateFile $NetworkingTemplateFile
+
+# Create Tanium Resource Group
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Force | Out-Null
 
-# Deploy Tanium 
+# Deploy Tanium
 New-AzureRmResourceGroupDeployment -Name "TaniumDeploy" `
                                    -ResourceGroupName $ResourceGroupName `
                                    -TemplateFile $TemplateFile `
